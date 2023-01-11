d36e0737aa6404c226cc7cb5cfca97a35eb07a0b
Sam Reynolds <sr848@cam.ac.uk>
2023-01-11 02:02:15 +0000

    removed unused imports

--- pages/about.js
+++ pages/about.js
@@ -3,8 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Header from '../components/header.js';
-import ExampleComponent from "react-rounded-image";
-import MyPhoto from '../public/Graph Website.png'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
